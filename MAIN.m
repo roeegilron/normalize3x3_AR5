6721102986f1c2a635d79e6eb97701992ad41611
Roee Gilron <roeegilron@gmail.com>
2016-05-02 10:13:45 +0300

    more files

---
+++ MAIN.m
@@ -0,0 +1,10 @@
+function MAIN()
+%% start up function to add paths of BVQX and SPM 
+startup_code()
+params = getparams();
+%% run normazlize again to save files at 3x3 resolution instead of 2x2 to make AR processing faster 
+
+%% run AR5 preprocessing 
+
+%% flaten data set 
+end
